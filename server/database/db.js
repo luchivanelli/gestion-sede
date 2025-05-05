c7502c65fb35b099fd312485f39b3948066c42a1
Luciana <lucianavanelli13gmail.com>
2025-05-05 20:03:38 -0300

    config db

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,10 @@
 import mysql2 from 'mysql2';
 
 export const connection = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'gestion_sede'
+    host: 'bfp9b7bqgwhm0nrgvgow-mysql.services.clever-cloud.com',
+    user: 'uomyffhmlqjv6gpx',
+    password: 'yl2CbWjZ6SGS5A8bGpj5',
+    database: 'bfp9b7bqgwhm0nrgvgow'
 });
 
 export const connectionDemo = mysql2.createConnection({
